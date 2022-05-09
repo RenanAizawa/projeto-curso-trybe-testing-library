f7496a330bfc10eacbab5f80a85b5ec2e9e20d70
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2022-05-09 16:59:25 -0500

    requesito 2 concluido, implementado teste de heading e img

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -6,11 +6,13 @@ import renderWithRouter from '../renderWithRouter';
 describe('Testando o componente About', () => {
   it('Teste se a pagina contém as informações sobre a Pokedex', () => {
     const { history } = renderWithRouter(<App />);
-    // const about = screen.getByRole('link', { name: /about/i });
-
     history.push('/about');
+    // const about = screen.getByRole('link', { name: /about/i });
+    const image = screen.getByAltText('Pokédex');
+    const url = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
 
     expect(screen.getByRole('heading', { name: /About Pokédex/i, level: 2 }))
       .toBeInTheDocument();
+    expect(image.src).toBe(url);
   });
 });
