7829b76a52107a9a99c8c67437a7346897c2859e
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2022-05-09 16:34:48 -0500

    erro de lint

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from '../services/renderWithRouter';
+
+describe('Testando o componente About', () => {
+  it('Teste se a pagina contém as informações sobre a Pokedex', () => {
+    const { history } = renderWithRouter(<App />);
+    const { location: { pathname } } = history;
+    // const about = screen.getByRole('link', { name: /about/i });
+
+    history.push('/about');
+
+    console.log(pathname);
+
+    // expect(screen.getByRole('heading'));
+  });
+});
