2ec6f10db98b969d1fa130196f044c51221c6bfa
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2022-05-09 17:45:09 -0500

    requesito 5 em construção

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import App from '../App';
+import renderwithRouter from '../renderWithRouter';
+
+describe('Tetando componente Pokedex', () => {
+  it('Tetando se heading existe na paginda', () => {
+    renderwithRouter(<App />);
+    const head = screen
+      .getByRole('heading', { name: /Encountered pokémons/i, level: 2 });
+    expect(head).toBeInTheDocument();
+  });
+  it('');
+});
