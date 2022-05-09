86de48fa3277b4b96f911c12ef582fda3aa1546c
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2022-05-09 16:02:14 -0500

    requesito 1 concluido, implementado teste de existencia dos link

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,18 @@
-test('iniciando o projeto', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from '../services/renderWithRouter';
+
+describe('Testando o componente App', () => {
+  it('Testando os links da aplicação', () => {
+    renderWithRouter(<App />);
+
+    const home = screen.getByRole('link', { name: /home/i });
+    const about = screen.getByRole('link', { name: /about/i });
+    const favorites = screen.getByRole('link', { name: /favorite pokémons/i });
+
+    expect(home).toBeInTheDocument();
+    expect(about).toBeInTheDocument();
+    expect(favorites).toBeInTheDocument();
+  });
+});
