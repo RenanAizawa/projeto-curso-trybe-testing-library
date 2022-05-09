0326dfe0c75272d2160f315709571597137942b0
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2022-05-09 17:33:31 -0500

    requesito 4 concluido, implemetando teste do h2 e image da pagina NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste do componente NotFound', () => {
+  it('Testando existencia do heading', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/1234');
+
+    const heading = screen
+      .getByRole('heading', { name: /Page requested not found/i, level: 2 });
+    const image = screen
+      .getByAltText('Pikachu crying because the page requested was not found');
+    const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    expect(heading).toBeInTheDocument();
+    expect(image.src).toBe(url);
+  });
+});
