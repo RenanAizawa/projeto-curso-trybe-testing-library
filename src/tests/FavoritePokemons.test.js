200afa7302923b586d4c5b2ff09cd57e1da98b99
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2022-05-09 17:14:20 -0500

    requesito 3 concluido

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testando componente FavoritePokemons', () => {
+  it('Teste h2 esta no documento', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/favorites');
+    const text = 'No favorite pokemon found';
+    const noFavorite = screen.getByText(text);
+
+    expect(noFavorite).toBeInTheDocument();
+  });
+});
