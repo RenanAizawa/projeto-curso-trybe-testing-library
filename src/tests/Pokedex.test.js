85f14ce3349fe9b4cfaa4343cc58197e90fee870
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2022-05-09 21:34:22 -0500

    requesito 5, 80% concluido

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,14 +1,24 @@
-import { screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import React from 'react';
 import App from '../App';
 import renderwithRouter from '../renderWithRouter';
 
 describe('Tetando componente Pokedex', () => {
-  it('Tetando se heading existe na paginda', () => {
+  it('Testando se heading existe na paginda', () => {
     renderwithRouter(<App />);
     const head = screen
       .getByRole('heading', { name: /Encountered pokémons/i, level: 2 });
     expect(head).toBeInTheDocument();
   });
-  it('');
+  it('Testando existência de todos os buttons', () => {
+    renderwithRouter(<App />);
+    const tipos = ['Electric', 'Fire', 'Bug', 'Poison', 'Psychic', 'Normal', 'Dragon'];
+    tipos
+      .map((tipo) => (expect(screen
+        .getByRole('button', { name: `${tipo}` })).toBeInTheDocument()));
+    fireEvent.click(screen.getByRole('button', { name: /All/i }));
+    expect(screen.getByRole('button', { name: /All/i })).toHaveBeenCalled();
+    expect(screen.getByRole('button', { name: /Próximo pokémon/i })).toBeInTheDocument();
+    expect(screen.getAllByTestId('pokemon-type-button')[0]).toBeInTheDocument();
+  });
 });
